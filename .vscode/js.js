609d0c0969ba6db235e9c75fd1cf7f04bc4452c0
jabo landry <arnoldjabo@gmail.com>
2024-04-10 13:59:02 +0200

    😎😎

---
+++ .vscode/js.js
@@ -0,0 +1,1 @@
+console.log("subscribe to j's development!!")
---
+++ js.js
@@ -0,0 +1,6 @@
+let openBnt =document.getElementById("btn")
+let closeBtn=document.getElementById("close")
+let modal=document.getElementById("modal")
+openBnt.addEventListener("click",()=>{
+    modal.classList.toggle("hide")
+})
